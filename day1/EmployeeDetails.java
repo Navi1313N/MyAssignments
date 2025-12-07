4ef05dddf8e82dfe4d0ed0ae911576080d3c8805
Navi1313N <naveenn8000@gmail.com>
2025-12-07 10:58:34 +0530

    Add files via upload
    Day 1 assignment uploaded

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "Naveen";
+		byte age = 26;
+		double salary = 55000.50;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 7813048510L;
+		int pinCode = 56001;
+		byte luckyNumber = 3;
+		
+		System.out.println("Welcome to Test Leaf Selenium Course");
+
+		System.out.println("Name: " + name + "\n" + "Age: " + age + "\n" + "Salary: " + salary + "\n" + "Gender: "
+				+ gender + "\n" + "AcceptTerms: " + acceptTerms + "\n" + "Rating: " + rating + "\n" + "Mobile No: "
+				+ mobileNumber + "\n" + "Pincode: " + pinCode + "\n" + "LuckyNumber: " + luckyNumber);
+
+	}
+
+}
